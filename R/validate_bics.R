56905de438d4e1276c4eb7ef874c2f00ecabc3a2
Tim Herbermann <tim.herbermann@embl.de>
2026-06-24 16:39:15 +0200

    simple metrics for bic training/test agreement in embedding space

---
+++ R/validate_bics.R
@@ -0,0 +1,69 @@
+
+
+.validate_overlap_bic <- function(
+  result_object,
+  embedding_t,
+  j
+) {
+
+  if (!("test_loading" %in% names(result_object$biclusters[[1]]))){ 
+    stop("No valid test data to compare to.")
+  }
+
+  cells_A <- result_object$biclusters[[j]]$cell_names
+  cells_B <- result_object$biclusters[[j]]$test_names
+
+  cells <- c(cells_A, cells_B)
+  X <- embedding_t[cells, , drop = FALSE]
+  group <- factor(ifelse(cells %in% cells_A, "A", "B"))
+
+  n <- min(length(cells_A), length(cells_B))
+  k <- min(floor(sqrt(n)), 50, n - 1)
+
+  nn <- FNN::get.knn(X, k = k)$nn.index
+  nn_group <- matrix(group[nn], nrow = nrow(nn))
+  mix_per_cell <- rowMeans(nn_group != group)
+
+  mix_score <- mean(mix_per_cell)
+
+  nA <- length(cells_A)
+  nB <- length(cells_B)
+
+  pA <- nA / (nA + nB)
+  expected <- 2 * pA * (1 - pA)
+
+  mix_score_norm <- mix_score / expected
+
+  pA_local <- rowMeans(nn_group == "A")
+  pB_local <- 1 - pA_local
+
+  lisi_per_cell <- 1 / (pA_local^2 + pB_local^2)
+  mean_lisi <- mean(lisi_per_cell)
+
+  return(c("kNN_mixing" = mix_score_norm, "LISI" = mean_lisi))
+}
+
+
+validate_overlap <- function(
+  result_object,
+  embedding
+) {
+
+  n_bic <- length(result_object$biclusters)
+  results <- data_frame(
+    bicluster = seq(n_bic),
+    kNN_mixing = numeric(n_bic),
+    LISI = numeric(n_bic)
+  )
+
+  embedding_t <- t(embedding)
+
+  for (i in seq(n_bic)){
+    temp <- .validate_overlap_bic(result_object, embedding_t, i)
+    results$kNN_mixing[i] <- temp[1]
+    results$LISI[i] <- temp[2]
+  }
+
+  results
+}
+
